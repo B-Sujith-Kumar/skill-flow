5ad80898541b6674737d83445b99745aaefe59dc
Banda Sujith Kumar <sujith.suave@gmail.com>
2024-04-07 16:28:54 +0530

    Added a link to view resume.

--- frontend/src/views/UpdateDetailsEmp.jsx
+++ frontend/src/views/UpdateDetailsEmp.jsx
@@ -316,6 +316,7 @@ const UpdateDetailsEmp = () => {
               data.employee.additionalInformation.skills.length !== 0
                 ? data.employee.additionalInformation.skills
                 : [],
+            resumeFile: data.employee.additionalInformation.resumeFile,
           });
         }
       } catch (err) {
@@ -538,7 +539,32 @@ const UpdateDetailsEmp = () => {
                     </div>
                   </div>
 
-                  {!resume ? (
+                  {formData.resumeFile ? (
+                    <div className="border-[2.5px] border-yellow-300 pb-6 px-7 pt-4 rounded-lg shadow-lg bg-[url('./assets/images/book-bg.png')] bg-white mt-8">
+                      <h2 className="text-xl font-semibold leading-7 text-gray-900">
+                        Resume Upload
+                      </h2>
+                      {/* <p className="mt-6">{formData.resumeFile}</p> */}
+                      <div className="mt-4 flex gap-2 text-blue-800">
+                        <a
+                          href={formData.resumeFile}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="text-blue-800"
+                        >
+                          Click here to view resume
+                        </a>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 448 512"
+                          width={15}
+                          fill="currentColor"
+                        >
+                          <path d="M429.6 92.1c4.9-11.9 2.1-25.6-7-34.7s-22.8-11.9-34.7-7l-352 144c-14.2 5.8-22.2 20.8-19.3 35.8s16.1 25.8 31.4 25.8H224V432c0 15.3 10.8 28.4 25.8 31.4s30-5.1 35.8-19.3l144-352z" />
+                        </svg>
+                      </div>
+                    </div>
+                  ) : !resume ? (
                     <div className="border-[2.5px] border-yellow-300 pb-12 px-7 pt-4 rounded-lg shadow-lg bg-[url('./assets/images/book-bg.png')] bg-white mt-8">
                       <h2 className="text-xl font-semibold leading-7 text-gray-900">
                         Resume Upload
