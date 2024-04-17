0583b35086981c83abfaebf37f996908cffaa241
Banda Sujith Kumar <sujith.suave@gmail.com>
2024-04-17 13:33:33 +0530

    Minor change in Employee Profile page

--- frontend/src/views/UpdateDetailsEmp.jsx
+++ frontend/src/views/UpdateDetailsEmp.jsx
@@ -342,68 +342,73 @@ const UpdateDetailsEmp = () => {
             Complete your profile
           </h2>
           <div className="flex w-full gap-8 mt-4 px-6 max-md:flex-col">
-            <div className="">
-              <div className="px-8 py-6 border-[3px] border-yellow-300 shadow-lg rounded-sm bg-white">
-                <img
-                  src={previewImg || formData.profileImage}
-                  alt=""
-                  style={{
-                    width: "200px",
-                    height: "200px",
-                    borderRadius: "50%",
-                    objectFit: "cover",
-                    display: "block",
-                    margin: "0 auto",
-                  }}
-                />
-                {!previewImg && (
-                  <div className="flex items-center justify-center mt-8 gap-2 bg-blue-800 px-2 py-2 rounded-sm text-white max-md:w-48 max-md:mx-auto">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md font-medium shadow-sm"
-                    >
-                      <span>Upload image</span>
-                      <input
-                        id="file-upload"
-                        name="file-upload"
-                        type="file"
-                        className="sr-only"
-                        accept=".jpg, .jpeg, .png"
-                        onChange={(e) => {
-                          setImg(e.target.files[0]);
-                          setPreviewImg(URL.createObjectURL(e.target.files[0]));
+            <div>
+              <div className="">
+                <div className="px-8 py-6 border-[3px] border-yellow-300 shadow-lg rounded-sm bg-white">
+                  <img
+                    src={previewImg || formData.profileImage}
+                    alt=""
+                    style={{
+                      width: "200px",
+                      height: "200px",
+                      borderRadius: "50%",
+                      objectFit: "cover",
+                      display: "block",
+                      margin: "0 auto",
+                    }}
+                  />
+                  {!previewImg && (
+                    <div className="flex items-center justify-center mt-8 gap-2 bg-blue-800 px-2 py-2 rounded-sm text-white max-md:w-48 max-md:mx-auto">
+                      <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer rounded-md font-medium shadow-sm"
+                      >
+                        <span>Upload image</span>
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          className="sr-only"
+                          accept=".jpg, .jpeg, .png"
+                          onChange={(e) => {
+                            setImg(e.target.files[0]);
+                            setPreviewImg(
+                              URL.createObjectURL(e.target.files[0])
+                            );
+                          }}
+                        />
+                      </label>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 640 512"
+                        width={25}
+                        fill="currentColor"
+                      >
+                        <path d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z" />
+                      </svg>
+                    </div>
+                  )}
+                  {previewImg && (
+                    <div className="flex gap-5 items-center justify-center mt-6">
+                      <button
+                        className="bg-blue-700 border-2 border-blue-700 px-5 py-1 text-white rounded-sm"
+                        onClick={uploadImage}
+                      >
+                        Submit
+                      </button>
+                      <button
+                        className="border-2 border-blue-700 px-5 py-1"
+                        onClick={() => {
+                          setPreviewImg(null);
                         }}
-                      />
-                    </label>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 640 512"
-                      width={25}
-                      fill="currentColor"
-                    >
-                      <path d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z" />
-                    </svg>
-                  </div>
-                )}
-                {previewImg && (
-                  <div className="flex gap-5 items-center justify-center mt-6">
-                    <button
-                      className="bg-blue-700 border-2 border-blue-700 px-5 py-1 text-white rounded-sm"
-                      onClick={uploadImage}
-                    >
-                      Submit
-                    </button>
-                    <button
-                      className="border-2 border-blue-700 px-5 py-1"
-                      onClick={() => {
-                        setPreviewImg(null);
-                      }}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                )}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  )}
+                </div>
               </div>
+              <div className="text-center mt-6 border-[3px] rounded-sm font-medium border-yellow-300 bg-white py-2 tracking-wide">Your ID : {formData.employeeID}</div>
             </div>
             <div className="flex-1">
               {formData && (
